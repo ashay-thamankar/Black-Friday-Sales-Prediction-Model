85e76fdd9fcf55f510ce6159f2f34830ced1b1a1
ashay-thamankar <ashaythamankar@gmail.com>
2023-12-31 00:39:19 +0530

    ingestion done

--- src/exception.py
+++ src/exception.py
@@ -16,9 +16,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by zero error")
-        raise CustomException(e, sys)
+
