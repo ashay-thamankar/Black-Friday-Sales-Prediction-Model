1b33c0ae1c4bdcbc6ed7017a9c2048c9aee410d1
ashay-thamankar <ashaythamankar@gmail.com>
2023-12-31 00:11:13 +0530

    utils added

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import dill
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
