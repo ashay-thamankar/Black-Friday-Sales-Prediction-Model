c9270a18a3f219032cfa6fe72a7e5328504f5407
ashay-thamankar <ashaythamankar@gmail.com>
2023-12-31 17:18:15 +0530

    workflow added

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def predict_datapoint():
         return render_template('home.html', results=round(results[0],3))
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True, port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
